88aad399f840b65a83cd324c4bd9e7d71be6a4e7
Your Full Name <youremailaddress@example.com>
2017-05-16 15:26:06 -0400

    Finished, working

--- index.js
+++ index.js
@@ -22,16 +22,20 @@ const App = {
         list.textContent = form.personName.value
         details.appendChild(list)
 
+        details.insertBefore(list, details.childNodes[0])
+
         const remove = document.createElement('button')
         remove.textContent = 'Remove'
         remove.style.backgroundColor = 'red'
         remove.style.marginLeft = '5px'
+        remove.style.float = 'right'
         list.appendChild(remove)
 
         const promote = document.createElement('button')
         promote.textContent = 'Promote'
         promote.style.backgroundColor = 'green'
         promote.style.marginLeft = '5px'
+        promote.style.float = 'right'
         list.appendChild(promote)
 
         remove.addEventListener('click', (ev) => this.onRemove(ev))
