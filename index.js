42d9a15794378086a602f429f4cb115e406dccf5
Your Full Name <youremailaddress@example.com>
2017-05-16 14:12:59 -0400

    added buttons

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const App = {
+    onRemove() {
+
+    },
+
+    onPromote() {
+
+    },
 
     runSubmit(ev) {
         ev.preventDefault()
@@ -8,6 +15,21 @@ const App = {
         const list = document.createElement('li')
         list.textContent = form.personName.value
         details.appendChild(list)
+
+        const remove = document.createElement('button')
+        remove.textContent = 'Remove'
+        remove.style.backgroundColor = 'red'
+        remove.style.marginLeft = '5px'
+        details.appendChild(remove)
+
+        const promote = document.createElement('button')
+        promote.textContent = 'Promote'
+        promote.style.backgroundColor = 'green'
+        promote.style.marginLeft = '5px'
+        details.appendChild(promote)
+
+        remove.addEventListener('click', () => this.onRemove)
+        promote.addEventListener('click', () => this.onPromote())
     },
 
     submit() { 
